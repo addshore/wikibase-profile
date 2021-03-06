e3c82e0a32f6d4001f9d146979abe1a95fec9396
addshore <addshorewiki@gmail.com>
2021-03-06 20:05:46 +0000

    Test with termed items

--- loads/1000EmptyItems.sh
+++ loads/1000EmptyItems.sh
@@ -1,5 +1,4 @@
-# Make 100 empty items...
-for i in {1..500}
+for i in {1..1000}
 do
   curl -s -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F 'new=item' -F 'data={}' http://localhost:8181/w/api.php >> process.out
 done

796151232ed0eb43f7aca1afb2041ebabfb202c1
addshore <addshorewiki@gmail.com>
2021-03-06 20:19:54 +0000

    More terms, and annotations

---
+++ loads/1000MultiTermItems.sh
@@ -0,0 +1,4 @@
+for i in {1..1000}
+do
+  curl -s -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F 'new=item' -F "data={\"labels\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}},\"descriptions\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}},\"aliases\":{\"en\":[{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}]}}" http://localhost:8181/w/api.php >> process.out
+done

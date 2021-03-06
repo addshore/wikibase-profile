ad974f445d9bb788ae51160eda48129973fdfd96
addshore <addshorewiki@gmail.com>
2021-03-06 21:41:58 +0000

    comment out some jobs & build localization cache pre run

--- loads/2000FullishItems.sh
+++ loads/2000FullishItems.sh
@@ -6,10 +6,7 @@
 properties=( wikibase-item wikibase-property string time )
 for p in "${properties[@]}"
 do
-  (
-    curl -s -m 20 -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F 'new=property' -F "data={\"datatype\":\"${p}\",\"labels\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}}}" http://localhost:8181/w/api.php
-  )&
-  if (( $(wc -w <<<$(jobs -p)) % 40 == 0 )); then wait; fi
+  curl -s -m 20 -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F 'new=property' -F "data={\"datatype\":\"${p}\",\"labels\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}}}" http://localhost:8181/w/api.php
 done
 
 # Create some items including statements

6a362e880b22ac48b24767017d47b28e97571baf
addshore <addshorewiki@gmail.com>
2021-03-07 12:34:01 +0000

    more async, less settings, less containers

--- loads/2000FullishItems.sh
+++ loads/2000FullishItems.sh
@@ -4,6 +4,9 @@ curl -s -m 20 -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F
 
 # Then do the bulk of the stuff
 
+# Divice ASYNC by 2, as we do 2 loops at the same time..
+ASYNC=$(expr $ASYNC / 2)
+
 # Create some properties
 # P2 = wikibase-item
 # P3 = wikibase-property
@@ -18,21 +21,21 @@ done
 # Create some items including statements
 
 # 1000 with strings
-for i in {1..1000}
+(for i in {1..1000}
 do
   (
     curl -s -m 20 -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F 'new=item' -F "data={\"labels\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}},\"descriptions\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}},\"aliases\":{\"en\":[{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}]},\"claims\":{\"P4\":[{\"mainsnak\":{\"snaktype\":\"value\",\"property\":\"P4\",\"datavalue\":{\"value\":\"statement-string-value\",\"type\":\"string\"},\"datatype\":\"string\"},\"type\":\"statement\",\"rank\":\"normal\"}]}}" http://localhost:818$((1 + RANDOM % $INSTANCES))/w/api.php
   )&
   if (( $(wc -w <<<$(jobs -p)) % $ASYNC == 0 )); then wait; fi
-done
+done)&
 
 # 1000 linking to other items
-for i in {1..1000}
+(for i in {1..1000}
 do
   (
     curl -s -m 20 -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F 'new=item' -F "data={\"labels\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}},\"descriptions\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}},\"aliases\":{\"en\":[{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}]},\"claims\":{\"P2\":[{\"mainsnak\":{\"snaktype\":\"value\",\"property\":\"P2\",\"datavalue\":{\"value\":{\"entity-type\":\"item\",\"numeric-id\":1,\"id\":\"Q1\"},\"type\":\"wikibase-entityid\"},\"datatype\":\"wikibase-item\"},\"type\":\"statement\",\"rank\":\"normal\"}]}}" http://localhost:818$((1 + RANDOM % $INSTANCES))/w/api.php
   )&
   if (( $(wc -w <<<$(jobs -p)) % $ASYNC == 0 )); then wait; fi
-done
+done)&
 
 wait
--- settings/lightweight.sh
+++ settings/lightweight.sh
@@ -14,3 +14,7 @@ echo "\$wgWBRepoSettings['tmpPropertySearchMigrationStage'] = MIGRATION_NEW;" >>
 # No recording of data in wb_changes table (for dispatching to clients)
 # https://doc.wikimedia.org/Wikibase/master/php/md_docs_topics_options.html#autotoc_md261
 echo "\$wgWBRepoSettings['useChangesTable'] = false;" >> LocalSettings.php
+
+# Use a seperate DB connection for ID allocation to reduce contention
+# https://doc.wikimedia.org/Wikibase/master/php/md_docs_topics_options.html#repo_idGeneratorSeparateDbConnection
+echo "\$wgWBRepoSettings['idGeneratorSeparateDbConnection'] = true;" >> LocalSettings.php

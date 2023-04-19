077c80cdb12c5361204939c8dc4e1b557088c3b2
addshore <addshorewiki@gmail.com>
2023-04-19 17:57:45 -0400

    Try and avoid fast import user error

--- loads/wbfastimport-2000full-batch100.sh
+++ loads/wbfastimport-2000full-batch100.sh
@@ -1,6 +1,7 @@
-# # Get the ball (and id counters rolling) with some simple entities..
-# curl -s -m 20 -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F 'new=item' -F 'data={}' http://localhost:818$((1 + RANDOM % $INSTANCES))/w/api.php >> process.out
-# curl -s -m 20 -X POST -F 'action=wbeditentity' -F 'format=json' -F 'token=+\' -F 'new=property' -F "data={\"datatype\":\"string\",\"labels\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}}}" http://localhost:818$((1 + RANDOM % $INSTANCES))/w/api.php >> process.out
+# Get the ball (and id counters rolling) with some simple entities..
+# Using wbfastimport for the item to try and get the error like https://phabricator.wikimedia.org/P47263
+curl -s -m 20 -X POST -F 'action=wbfastimport' -F 'format=json' -F 'data={\"type\":\"item\"}' http://localhost:818$((1 + RANDOM % $INSTANCES))/w/api.php >> process.out
+curl -s -m 20 -X POST -F 'action=wbeditentity' -F 'format=json' -F 'new=property' -F "data={\"datatype\":\"string\",\"labels\":{\"en\":{\"language\":\"en\",\"value\":\"$(sed "s/[^a-zA-Z0-9]//g" <<< $(openssl rand -base64 30))\"}}}" http://localhost:818$((1 + RANDOM % $INSTANCES))/w/api.php >> process.out
 
 # # Create some properties
 # # P2 = wikibase-item
